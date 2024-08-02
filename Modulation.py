b54b07b082c70bc9f863a7eba06b3e26e9cc686b
Ryan <ryhime1@gmail.com>
2024-08-02 19:50:13 -0400

    Demod BASK if thats what its called...

--- Modulation.py
+++ Modulation.py
@@ -1,22 +1,32 @@
 import numpy as np
-import matplotlib.pyplot as plt 
+import matplotlib.pyplot as plt
+s = .001
 def modulateBASK(bitStream: str, symbolDuration = 1, fc = 1/2):
-    s = .001
+    print(len(bitStream))
     totalDuration = symbolDuration * len(bitStream)
-    x = np.arange(1, totalDuration, step=s)
+    x = np.arange(0, totalDuration, step=s)
     y = []
     lastInterval = 0
     for i in range(len(bitStream)):
         interval = symbolDuration*int(1/s)*(i+1)
         y.extend(np.sin(2*np.pi*fc*x[lastInterval:interval])/(2 if bitStream[i]=='0' else 1))
         lastInterval = interval
+    return y
 
 
-    plt.plot(x, y)
-    plt.show()
+def demodulateBASK(wave: list, symbolDuration = 1) -> str:
+    numberBits = len(wave)//(symbolDuration*int(1/s))
+    bitsFound = "";
+    lastInterval = 0
+    for i in range(numberBits):
+        interval = symbolDuration*int(1/s)*(i+1)
+        maximumValue = max(wave[lastInterval:interval])
+        if (abs(maximumValue)<=10**(-3)): raise Exception("Frequency Not High Enough to Demodulate")
+        bitsFound+="1" if abs(maximumValue-1)<=abs(maximumValue-.5) else "0"
+        lastInterval = interval
+    return bitsFound
 
 def modulatePBSK(bitStream: str, symbolDuration = 1, fc = 1/2):
-    s = .001
     totalDuration = symbolDuration * len(bitStream)
     x = np.arange(1, totalDuration, step=s)
     y = []
@@ -25,10 +35,14 @@ def modulatePBSK(bitStream: str, symbolDuration = 1, fc = 1/2):
         interval = symbolDuration*int(1/s)*(i+1)
         y.extend(np.sin(2*np.pi*fc*x[lastInterval:interval]+(np.pi if bitStream[i]=='1' else 0)))
         lastInterval = interval
+    return y
 
 
-    plt.plot(x, y)
-    plt.show()
+bits = "1001010101010101111010110"
+modulated = modulateBASK(bits, fc=5)
+demoded = demodulateBASK(modulated)
 
+print(demoded==bits)
 
-modulatePBSK('0000001111100011')
+plt.plot(np.arange(len(modulated)),modulated)
+plt.show()
